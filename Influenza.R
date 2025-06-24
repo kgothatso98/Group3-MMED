39338232b2ff7d381b1a2e6d8865357fa0d0bf37
giftkga13 <gift@aims.ac.za>
2025-06-24 17:10:57 +0200

    Add files via upload
    This is the updated version of the first one. There is an additional code and the first code is updated.

--- Influenza.R
+++ Influenza.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-
 CapeFLU <- readRDS("CapeFLU.rds")
 CapeFLU$death_date <- as.Date(CapeFLU$death_date)
 CapeFLU$death_date
@@ -24,4 +23,32 @@ ggplot(FLUepicurve, aes(x = death_date, y = n)) +
   theme_minimal() +
   theme(
     axis.text.x = element_text(angle = 0, vjust = 0.5)
+  )
+FLUepicurve <- FLUepicurve %>%
+  mutate(EpiWeek=isoweek(death_date))
+FLUepicurve <- FLUepicurve %>%
+  mutate(EpiYear=year(death_date))
+FluepiWeek <- FLUepicurve %>% 
+  group_by(EpiYear, EpiWeek) %>% 
+  summarise(n = sum(n)) %>% 
+  ungroup()
+FluepiWeek <- FluepiWeek %>% mutate(Weeknum = seq(1,24,1))
+View(FluepiWeek)
+
+
+FluepiWeek$Year <- ifelse(FluepiWeek$Weeknum <= 19, "1918", "1919")
+FluepiWeek$WeekLabel <- paste(FluepiWeek$Year, "- W", FluepiWeek$Weeknum, sep = "")
+FluepiWeek$WeekLabel <- factor(FluepiWeek$WeekLabel, levels = FluepiWeek$WeekLabel)
+ggplot(FluepiWeek, aes(x = WeekLabel, y = n, fill = Year)) +
+  geom_col() +
+  scale_fill_manual(values = c("1918" = "lightblue", "1919" = "blue")) +
+  labs(
+    x = "Week",
+    y = "Number of Weekly Deaths",
+    fill = "Year",
+    title = "Pneumonia/Influenza Weekly Deaths (1918–1919)"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90, vjust = 0.5, size = 8)
   )
